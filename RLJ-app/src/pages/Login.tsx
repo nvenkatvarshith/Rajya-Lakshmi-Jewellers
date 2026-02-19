18c8053557f8c38a185d077379d11a063a48f7fc
nvenkatvarshith <venkatvarshith2@gmail.com>
2026-02-19 13:22:46 +0530

    added validation for login page

--- RLJ-app/src/pages/Login.tsx
+++ RLJ-app/src/pages/Login.tsx
@@ -5,6 +5,10 @@ import {Link} from 'react-router-dom'
 function Login(){
     const { register, handleSubmit, formState:{errors}, watch} = useForm();
 
+    const login = (formValues:any) => {
+        console.log(formValues);
+    };
+
     return (
         <div className="container my-3">
             <div className="d-flex justify-content-center column-gap-4">
@@ -12,28 +16,41 @@ function Login(){
                 <h4 className="text-uppercase"><Link to="/signup" className='text-dark text-decoration-none'>Create Your Account</Link></h4>
             </div>
 
-            <div className="w-50 p-5 mx-auto mt-4" id='login-form'>
-                <div className="px-5">
-                    <p>If you are already registered with Cartier, login here:</p>
-                    <p className='m-0 fw-lighter text-end'>Required field*</p>
-                    <div className="form-floating mb-3 border-bottom border-dark shadow-none">
-                        <input type="email" className="form-control" id="emailLogin" placeholder="name@example.com"/>
-                        <label htmlFor="emailLogin" className='ps-0'>Email address<span>*</span></label>
-                    </div>
+            <form onSubmit={handleSubmit(login)}>
+                <div className="w-50 p-5 mx-auto mt-4" id='login-form'>
+                    <div className="px-5">
+                        <p>If you are already registered with RLJ, login here:</p>
+                        <p className='m-0 fw-lighter text-end'>Required field*</p>
+                        <div className="form-floating mb-3 border-bottom border-dark shadow-none">
+                            <input type="text" className="form-control" id="emailLogin" placeholder="name@example.com"
+                                {...register('email',{
+                                    required: {value:true, message:'Email is required'},
+                                    pattern: {value:/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,}$/,message:'Incorrect Email Format'}
+                                })}
+                                />
+                            <label htmlFor="emailLogin" className='ps-0'>Email address<span>*</span></label>
+                        </div>
+                        {errors.email?.message && <p className="text-danger small m-0">{String(errors.email.message)}</p>}
 
-                    <div className="form-floating mb-3 border-bottom border-dark shadow-none">
-                        <input type="password" className="form-control" id="password" placeholder="..."/>
-                        <label htmlFor="password" className='ps-0'>Password<span>*</span></label>
+                        <div className="form-floating mb-3 border-bottom border-dark shadow-none">
+                            <input type="password" className="form-control" id="password" placeholder="..." 
+                                {...register('password',{
+                                    required: {value:true, message:'Password is required'}
+                                })}
+                            />
+                            <label htmlFor="password" className='ps-0'>Password<span>*</span></label>
+                        </div>
+                        {errors.password?.message && <p className="text-danger small m-0">{String(errors.password.message)}</p>}
                     </div>
+                    <div className='fw-lighter small ps-5'>
+                        <h6 className='py-2 text-decoration-underline'>Forgot your password?</h6>
+                        <p className='pb-2'>Read the <span className='text-decoration-underline'>Privacy Policy</span> for further information</p>
+                        <div className='text-center'>
+                            <button type='submit' className='btn btn-dark text-uppercase rounded-0 px-4'>Login</button>
+                        </div>
+                    </div>                
                 </div>
-                <div className='fw-lighter small ps-5'>
-                    <h6 className='py-2 text-decoration-underline'>Forgot your password?</h6>
-                    <p className='pb-2'>Read the <span className='text-decoration-underline'>Privacy Policy</span> for further information</p>
-                    <div className='text-center'>
-                        <button className='btn btn-dark text-uppercase rounded-0 px-4'>Login</button>
-                    </div>
-                </div>                
-            </div>
+            </form>
         </div>
     )
 }
